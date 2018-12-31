f88187b346f62051226552048784b8522c6d36a7
Shivan Raptor <findme@raptor.hk>
2018-12-31 15:43:31 +0800

    Create config.py
    The database config file

---
+++ config.py
@@ -0,0 +1,6 @@
+# Don't forget to change these MySQL DB connection settings
+class Config:
+	DB_NAME = 'YourSchemaName'
+	DB_HOST = 'localhost'
+	DB_USER = 'YourDBUserName'
+	DB_PASS = 'YourDBPassword'
